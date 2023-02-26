516137ebf7e13a706526ea5f42691a403c6db9b2
bora-jeong97 <57898425+bora-jeong97@users.noreply.github.com>
2023-02-26 15:27:43 +0900

    [Bronze V] Title: A×B, Time: 204 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args){
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        System.out.println(a*b);
+    }
+}
