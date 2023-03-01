8f1414f0136187321f2ce88949c2b28b276c8c22
bora-jeong97 <57898425+bora-jeong97@users.noreply.github.com>
2023-03-01 17:37:51 +0900

    [Bronze V] Title: 꼬마 정민, Time: 216 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args){
+        long a = s.nextLong();
+        long b = s.nextLong();
+        long c = s.nextLong();
+        System.out.print(a+b+c);
+    }
+}
