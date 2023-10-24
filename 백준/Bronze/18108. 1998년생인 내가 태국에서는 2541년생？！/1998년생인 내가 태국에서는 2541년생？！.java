8484d05668bfafffd8a319ed69f1ef9b2098cb47
bora-jeong97 <57898425+bora-jeong97@users.noreply.github.com>
2023-10-24 10:43:41 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int minus = 2541 - 1998;
+        int input = sc.nextInt();
+
+        System.out.println(input - minus);
+    }
+
+}
