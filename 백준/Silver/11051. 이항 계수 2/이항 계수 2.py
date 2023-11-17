27fea6ae833c448cf0c64c9920d838d0ae983e93
bora-jeong97 <57898425+bora-jeong97@users.noreply.github.com>
2023-11-17 19:20:52 +0900

    [Silver II] Title: 이항 계수 2, Time: 300 ms, Memory: 53780 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -1,23 +1,15 @@
-import sys
-sys.setrecursionlimit(10 ** 6)
+'''이항계수'''
 MOD = 10007
-
-
 
 cache = [[0] * 1001 for _ in range(1001)]   # 최대값까지 안나오는 값으로 초기화
 N, K = map(int, input().split())
 
+# 반복문
+for i in range(1001):
+    cache[i][0] = cache[i][i] = 1
+    for j in range(1, i):
+        cache[i][j] = cache[i-1][j-1] + cache[i-1][j]
+        cache[i][j] %= MOD
 
-# 재귀
-def bino(n, k):
-    if cache[n][k]:
-        return cache[n][k]
 
-    if k == 0 or k == n :   # 무한히 반복되는 것을 막기 위한 이항계수 정의
-        cache[n][k] = 1
-    else:
-        cache[n][k] = bino(n - 1, k - 1) + bino(n - 1, k)
-
-    return cache[n][k]
-
-print(bino(N, K) % 10007)
+print(cache[N][K])
