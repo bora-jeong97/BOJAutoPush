4332385fe8ca9e950c7745114029247e946f5959
bora-jeong97 <57898425+bora-jeong97@users.noreply.github.com>
2023-11-18 17:33:47 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 168 ms, Memory: 60,956 KB -BaekjoonHub

---
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -0,0 +1,23 @@
+
+# 총 10번 반복
+for test_cast in range(1, 11):
+    result = 0
+    N = int(input())    # 덤프횟수
+    lst = list(map(int, input().split()))  # 집들의 높이
+
+    mx = 0
+    mn = 0
+    # 덤프 횟수 만큼 반복
+    for i in range(N):
+        mx = max(lst)
+        mn = min(lst)
+        maxIndex = lst.index(mx)
+        minIndex = lst.index(mn)
+        if mx - mn > 1:
+            lst[maxIndex] -= 1
+            lst[minIndex] += 1
+
+    mx = max(lst)
+    mn = min(lst)
+    #print(f"#{test_cast} max : {mx} min : {mn} result : {mx-mn}")
+    print(f"#{test_cast} {mx-mn}")
